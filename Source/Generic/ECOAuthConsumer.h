d456d4d403111c8fe580b763d305624b689d50ba
Sam Deane <sam@elegantchaos.com>
2012-08-23 09:34:52 +0100

    added imports

--- Source/Generic/ECOAuthConsumer.h
+++ Source/Generic/ECOAuthConsumer.h
@@ -4,3 +4,6 @@
 //  liberal license: http://www.elegantchaos.com/license/liberal
 // --------------------------------------------------------------------------
 
+#import "OAToken.h"
+#import "OAConsumer.h"
+#import "OAMutableURLRequest.h"
